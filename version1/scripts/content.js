6eb47c812fb4a7e5f7c77636620caa7e7331df22
PrinceTyagia <veertyagia@gmail.com>
2023-11-04 17:49:45 +0530

    inital setup folder

---
+++ version1/scripts/content.js
@@ -0,0 +1,6 @@
+let ulHeader = document.querySelector("ul.global-nav__primary-items");
+let liViewPosts = document.createElement("li");
+// liViewPosts.innerHTML = "test";
+ulHeader.appendChild(liViewPosts);
+liViewPosts.classList.add("global-nav__primary-item");
+liViewPosts.innerHTML = "test";
